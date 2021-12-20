a38547651fbe22bdadf5eacfd6a3ce82067046ce
Zack Siri <zack@artellectual.com>
2021-12-20 21:12:26 +0700

    add assertion

--- lib/uplink.ex
+++ lib/uplink.ex
@@ -1,19 +1,3 @@
 defmodule Uplink do
-  @moduledoc """
-  Documentation for Uplink.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Uplink.hello()
-      :world
-
-  """
-
-  def hello do
-    :world
-  end
 end
--- test/uplink/packages/deployment/router_test.exs
+++ test/uplink/packages/deployment/router_test.exs
@@ -4,7 +4,7 @@ defmodule Uplink.Packages.Deployment.RouterTest do
 
   alias Uplink.{
     Packages,
-    Members
+    Members,
   }
 
   alias Packages.Deployment.Router
@@ -24,7 +24,7 @@ defmodule Uplink.Packages.Deployment.RouterTest do
 
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Uplink.Repo)
-
+        
     {:ok, _actor} =
       Members.create_actor(%{
         identifier: "zacksiri"
@@ -46,5 +46,6 @@ defmodule Uplink.Packages.Deployment.RouterTest do
       |> Router.call(@opts)
 
     assert conn.status == 201
+    assert %{"data" => %{"id" => _deployment_id}} = Jason.decode!(conn.resp_body)
   end
 end
