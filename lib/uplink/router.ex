fe2815dc27234d73b0d0bdf9eac4e97db68d854d
Zack Siri <zack@artellectual.com>
2022-01-03 15:57:31 +0700

    Add static path to router

--- lib/uplink/router.ex
+++ lib/uplink/router.ex
@@ -1,10 +1,15 @@
 defmodule Uplink.Router do
   use Plug.Router
+  
+  plug Plug.Static,
+    at: "/distribution",
+    from: "tmp/deployments"
 
   plug :match
   plug :dispatch
 
   alias Uplink.Packages.Deployment
+
 
   forward("/deployments", to: Deployment.Router)
 end
