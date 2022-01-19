f64fda32de5965a21b32b46db754b3b4eb168a60
Zack Siri <zack@artellectual.com>
2022-01-19 22:01:45 +0700

    Add aliases

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Uplink.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
