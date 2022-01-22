44791ed5d0370811ecba3b892bb13893d9577a7b
Zack Siri <zacksiri@gmail.com>
2022-01-22 23:30:54 +0700

    clean up formatting

--- lib/uplink/clients/lxd/instance.ex
+++ lib/uplink/clients/lxd/instance.ex
@@ -1,7 +1,7 @@
 defmodule Uplink.Clients.LXD.Instance do
   use Ecto.Schema
   import Ecto.Changeset
-  
+
   @valid_attrs ~w(
     name
     type
@@ -14,7 +14,7 @@ defmodule Uplink.Clients.LXD.Instance do
     last_used_at
     expanded_config
   )a
-  
+
   @required_attrs ~w(
     name
     type
@@ -25,7 +25,7 @@ defmodule Uplink.Clients.LXD.Instance do
     created_at
     last_used_at
   )a
-  
+
   @primary_key false
   embedded_schema do
     field :name, :string
@@ -35,20 +35,20 @@ defmodule Uplink.Clients.LXD.Instance do
     field :architecture, :string
     field :profiles, {:array, :string}
     field :description, :string
-    
+
     field :created_at, :utc_datetime_usec
     field :last_used_at, :utc_datetime_usec
-      
+
     field :expanded_config, :map
     field :expanded_devices, :map
   end
-  
+
   def changeset(schema, params) do
     schema
     |> cast(params, @valid_attrs)
     |> validate_required(@required_attrs)
   end
-  
+
   def parse(params) do
     %__MODULE__{}
     |> changeset(params)
