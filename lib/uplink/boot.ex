c9ff5413903611cfaa95705d91b45e1292276305
Zack Siri <zacksiri@gmail.com>
2022-04-19 22:07:14 +0700

    Add test for boot

---
+++ lib/uplink/boot.ex
@@ -0,0 +1,17 @@
+defmodule Uplink.Boot do
+  use Task
+  
+  alias Uplink.Clients.Instellar
+  
+  require Logger
+  
+  def start_link(args) do
+    Task.start_link(__MODULE__, :run, [args])
+  end
+  
+  def run(_args) do
+    Logger.info("[Boot] Establishing uplink...")
+    
+    Instellar.Register.perform()
+  end
+end
