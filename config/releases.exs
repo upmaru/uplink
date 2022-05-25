71e09574292bae0eec1886d4e47b29b9ea35a285
Zack Siri <zacksiri@gmail.com>
2022-05-25 21:55:25 +0700

    Add config for instellar endpoint

--- config/releases.exs
+++ config/releases.exs
@@ -2,6 +2,10 @@ import Mix.Config
 
 config :uplink, Uplink.Repo, url: System.get_env("DATABASE_URL")
 
+config :uplink, Uplink.Clients.Instellar,
+  endpoint:
+    System.get_env("INSTELLAR_ENDPOINT", "https://web.instellar.app/uplink")
+
 config :uplink, Uplink.Cluster,
   installation_id: System.get_env("UPLINK_INSTALLATION_ID")
 
