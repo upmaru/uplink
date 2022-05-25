418076aac949a0ab07303e0c6eb1a45d04b1c10c
Zack Siri <zacksiri@gmail.com>
2022-05-25 21:05:26 +0700

    update releases config

--- config/releases.exs
+++ config/releases.exs
@@ -1,1 +1,9 @@
 import Mix.Config
+
+config :uplink, Uplink.Repo,
+  url: System.get_env("DATABASE_URL")
+
+config :uplink, Uplink.Cluster,
+  installation_id: System.get_env("UPLINK_INSTALLATION_ID")
+
+config :uplink, Uplink.Secret, System.get_env("UPLINK_SECRET")
