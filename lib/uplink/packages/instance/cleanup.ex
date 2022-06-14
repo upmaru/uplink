8dc629ba79cbd41eed4165c18ecd88d614386230
Zack Siri <me@zacksiri.dev>
2022-06-14 21:19:55 +0700

    Add cleanup module
    Solves #16

---
+++ lib/uplink/packages/instance/cleanup.ex
@@ -0,0 +1,38 @@
+defmodule Uplink.Packages.Instance.Cleanup do
+  use Oban.Worker, queue: :process_instance, max_attempts: 1
+
+  alias Uplink.{
+    Clients,
+    Packages,
+    Repo
+  }
+
+  alias Clients.{
+    LXD,
+    Instellar
+  }
+
+  alias Packages.Install
+
+  def perform(%Oban.Job{
+        args: %{
+          "instance" => %{
+            "slug" => name,
+            "node" => %{
+              "slug" => _node_name
+            }
+          },
+          "install_id" => install_id
+        }
+      }) do
+    client = LXD.client()
+    %Install{} = install = Repo.get(Install, install_id)
+
+    with {:ok, _} <- Formation.Lxd.stop(client, name),
+         {:ok, _} <- Formation.Lxd.delete(client, name) do
+      Instellar.transition_instance(name, install, "fail",
+        comment: "[Uplink.Packages.Instance.Cleanup]"
+      )
+    end
+  end
+end
