22704564b99ef3c11abb1e44aa3fa9a6795c1c18
Zack Siri <me@zacksiri.dev>
2023-05-18 22:38:33 +0700

    slight clean up

--- lib/uplink/packages/instance/upgrade.ex
+++ lib/uplink/packages/instance/upgrade.ex
@@ -72,6 +72,7 @@ defmodule Uplink.Packages.Instance.Upgrade do
          formation_instance,
          %Install{
            id: install_id,
+           instellar_installation_id: instellar_installation_id,
            deployment: incoming_deployment
          } = install
        ) do
@@ -79,7 +80,7 @@ defmodule Uplink.Packages.Instance.Upgrade do
     |> where(
       [i],
       i.id != ^install_id and
-        i.instellar_installation_id == ^install.instellar_installation_id
+        i.instellar_installation_id == ^instellar_installation_id
     )
     |> order_by(desc: :inserted_at)
     |> preload([:deployment])
