b4b9b29d8657a032267e9e8763105267e36c77c3
Zack Siri <me@zacksiri.dev>
2023-12-14 11:56:25 +0700

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Uplink.MixProject do
   def project do
     [
       app: :uplink,
-      version: "0.10.1",
+      version: "0.10.2",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
