23890c79279110435362b1e1a23d76395ec11111
Zack Siri <me@zacksiri.dev>
2024-01-08 14:12:59 +0700

    set database config when fallback to lite

--- lib/uplink/data/provisioner.ex
+++ lib/uplink/data/provisioner.ex
@@ -135,6 +135,14 @@ defmodule Uplink.Data.Provisioner do
         Application.put_env(:uplink, Uplink.Repo, url: db_url)
         GenServer.stop(conn)
 
+        database_url =
+          Formation.Lxd.Alpine.postgresql_connection_url(scheme: "ecto")
+
+        Application.put_env(:uplink, Uplink.Repo,
+          url: database_url,
+          pool_size: 2
+        )
+
         Uplink.Release.Tasks.migrate(force: true)
         Uplink.Data.start_link([])
 
