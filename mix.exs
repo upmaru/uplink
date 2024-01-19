25e0045d1f53bf0af21ebb151cadd56ac45418b4
Zack Siri <me@zacksiri.dev>
2024-01-19 16:30:10 +0700

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Uplink.MixProject do
   def project do
     [
       app: :uplink,
-      version: "0.12.3",
+      version: "0.12.4",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
