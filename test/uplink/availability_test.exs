f4734896339f332c1cbb25b57cf3c0e759c996b1
Zack Siri <me@zacksiri.dev>
2025-01-09 16:51:33 +0700

    Setup test case and bypass for uplink nodes listing

--- test/uplink/availability_test.exs
+++ test/uplink/availability_test.exs
@@ -1,2 +1,38 @@
 defmodule Uplink.AvailabilityTest do
+  use ExUnit.Case
+
+  alias Uplink.Availability
+
+  setup do
+    bypass = Bypass.open()
+
+    Cache.put(:self, %{
+      "credential" => %{
+        "endpoint" => "http://localhost:#{bypass.port}"
+      }
+    })
+
+    response = File.read!("test/fixtures/lxd/cluster/members/list.json")
+
+    Cache.delete(:cluster_members)
+
+    {:ok, bypass: bypass}
+  end
+
+  describe "check availability of the nodes in the cluster" do
+    test "return availability check result", %{
+      bypass: bypass,
+      response: response
+    } do
+      Bypass.expect_once(bypass, "GET", "/1.0/cluster/members", fn conn ->
+        assert %{"recursion" => "1"} = conn.query_params
+
+        conn
+        |> Plug.Conn.put_resp_header("content-type", "application/json")
+        |> Plug.Conn.resp(200, response)
+      end)
+
+      assert {:ok, result} = Availability.check!()
+    end
+  end
 end
